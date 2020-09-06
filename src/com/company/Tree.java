1aead4179f9c084314f4b89d6c11d416bee453a0
Denis Rybas <denis2002.rybas@gmail.com>
2020-09-06 16:52:22 +0300

    Tree draw

---
+++ src/com/company/Tree.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.awt.*;
+
+public class Trunk implements Drawable {
+
+    @Override
+    public void draw(Graphics2D g) {
+        g.setColor(Color.GREEN);
+        drawTrunk();
+    }
+
+    private void drawTrunk() {
+
+    }
+}
